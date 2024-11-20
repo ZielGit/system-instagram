1aa955d0253286a01a6dd4323ef7e7a96a3fbd5a
Frans <frans_421_12@hotmail.com>
2024-11-19 19:24:07 -0500

    chore: update tailwind config to streamline content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-
-    "./resources/**/*.blade.php",
-    "./resources/**/*.blade.js",
-    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-
-
-
-
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
   ],
   theme: {
     extend: {},
